decc0843311cf57728647d5681819fc48b33220a
d4n11083 <d4n11083@estudiantec.cr>
2018-09-26 12:37:17 -0600

    Cambios en el cliente, y utilizacion de Json

--- MPointer.h
+++ MPointer.h
@@ -5,32 +5,109 @@
 #ifndef MPOINTER_MPOINTER_H
 #define MPOINTER_MPOINTER_H
 
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <ctime>
 
 
 template <typename T>
 class MPointer {
 private:
-    T* puntero = nullptr;
+
+    T* puntero = NULL;
+    T valor = 0;
+    int idMpointer;
+    int idMpointerGB;
+    int idMpointerServer;
+
+    static int cantidadMPointers;
 
 public:
+
     MPointer();
     static MPointer<T> New();
-
+    T operator*(T pMpointer);
+    T operator&();
+    T operator=( T elemento1);
 };
 
+/**
+ * Inicialización del entero cantidadMPointers como variable estatica para conocer la cantidad
+ * de MPointer en cualquier momento.
+ * @tparam T
+ */
 template <typename T>
-MPointer<T>::MPointer() {
-    puntero = (T*)calloc(2, sizeof(T));
-    std::cout << "Puntero Construido" << std::endl;
-}
+int MPointer<T>::cantidadMPointers = 0;
 
-
+/**
+ * LLama al constructor de la clase.
+ * @tparam T tipo de dato
+ * @return
+ */
 template <typename T>
 MPointer<T> MPointer<T>::New(){
     MPointer();
     std::cout <<"Se ha creado un nuevo puntero..." << std::endl;
 }
+
+/**
+ * Inicializa el puntero, añade un ID al MPointer
+ * @tparam T tipo de dato que va a ser guardado
+ */
+
+template <typename T>
+MPointer<T>::MPointer() {
+    puntero = (T*)calloc(1, sizeof(T));
+    idMpointer = cantidadMPointers;
+    cantidadMPointers++;
+    std::cout << "Puntero Construido" << std::endl;
+}
+
+/**
+ * Sobrecarga del operador "="
+ * Verifica el  tipo de los operandos
+ *
+ * @tparam T tipo de dato
+ * @param elemento1 elemento a la izquierda de =
+ * @param elemento2 elemento a la derecha de =
+ * @return
+ */
+template<typename T>
+T MPointer<T>::operator=(T elemento1){
+
+
+
+}
+
+
+template <typename T>
+T MPointer<T>::operator*( T pMpointer) {
+    if(puntero != NULL){
+        valor = pMpointer;
+        puntero = &valor;
+        std::cout << "Puntero Guardado" << std::endl;
+        std::cout << "COUT DE PRUEBA, Imprimiendo el dato guardado: "<< *puntero << std::endl;
+    }else {
+        std::cout << "Puntero Nulo " << std::endl;
+    }
+}
+
+template <typename T>
+T MPointer<T>::operator&() {
+    if (puntero == nullptr){
+        std::cout << "Es Nulo" << std::endl;
+        return 0;
+    }else{
+        return *puntero;
+    }
+}
+
+
+
+
+
+
 #endif //MPOINTER_MPOINTER_H
--- main.cpp
+++ main.cpp
@@ -1,14 +1,64 @@
 #include <iostream>
 #include "Cliente/Cliente.h"
 #include "MPointer.h"
+#include "typeinfo"
+#include "Cliente/JSON/json.hpp"
+
+using json =  nlohmann::json;
+
 int main() {
 
-    MPointer<int > ptr = MPointer<int >::New();
-    std::cout << "###################" << std::endl;
+    int a = 0;
+    char t = 'e';
+
+    //MPointer<int > ptr  = MPointer<int >::New();
+    //MPointer<int > ptr1 = MPointer<int >::New();
+    //MPointer<int > ptr3 = MPointer<int >::New();
+
+    json solicitud;
+
+    solicitud["Solicitud"] = 1;
+    std::string stringEnviar = solicitud.dump();
+    char *mensajejsonchar = &stringEnviar[0u];
+
+
+
+
+    //std::cout << typeid(ptr).name() << std::endl;
+
+
+//    MPointer<int > ptr = MPointer<int >::New();
+//    std::cout << "###################" << std::endl;
+//
+//    ptr.operator*(7);
+//    std::cout << ptr.operator&() << std::endl;
+//
+//    std::cout << ptr.operator&() << std::endl;
+//    std::cout << ptr.operator&() << std::endl;
+//
+//    ptr.operator*(4);
+//    std::cout << ptr.operator&() << std::endl;
+//
+//
+
+
+//      std::cout << ptr.operator&() << std::endl;
+
+//    int z = ptr.operator&();
+//    std::cout<< "Imprimiendo z" << std::endl;
+//    std::cout << z;
 
     Cliente* cliente = new Cliente(sockaddr_in());
     cliente->iniciar();
-    cliente->enviaDatos("Este mensaje fue enviado desde el cliente");
+    cliente->enviaDatos(mensajejsonchar);
     std::cout << cliente->recibe(1024);
+    std::cout << "\n" <<std::endl;
+    cliente->enviaDatos(mensajejsonchar);
+
+    std::cout << cliente->recibe(1024);
+    cliente->enviaDatos("HOLA MUNDO 1 OMG");
+    std::cout << cliente->recibe(1024);
+
+
     return 0;
 }
